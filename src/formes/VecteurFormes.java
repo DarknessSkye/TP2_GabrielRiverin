fcf6f642adc73c7a24eacfe55cf26bbb04e0be72
Gabriel Riverin <gabkanuna@gmail.com>
2023-02-22 10:30:48 -0500

    Ajout de la Classe VecteurFormes

---
+++ src/formes/VecteurFormes.java
@@ -0,0 +1,14 @@
+/*
+420-202 – TP2 – Traitement de données orienté objet
+Groupe : 1 lundi & mercredi
+Nom : Riverin
+Prénom : Gabriel
+DA : 2244454
+Lien GIT Hub : https://github.com/DarknessSkye/TP2_GabrielRiverin/commits/main
+ */
+
+package formes;
+
+public class VecteurFormes {
+    
+}
