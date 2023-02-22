5ca5c9059e1350e0fe63758d77a2d42c735880de
Gabriel Riverin <gabkanuna@gmail.com>
2023-02-22 10:28:27 -0500

    Ajout de la méthode toStringCourt

--- src/exceptions/FormeException.java
+++ src/exceptions/FormeException.java
@@ -1,9 +1,10 @@
 /*
-420-202 – TP1 – Traitement de données orienté objet
+420-202 – TP2 – Traitement de données orienté objet
 Groupe : 1 lundi & mercredi
 Nom : Riverin
 Prénom : Gabriel
 DA : 2244454
+Lien GIT Hub : https://github.com/DarknessSkye/TP2_GabrielRiverin/commits/main
  */
 
 package exceptions;
